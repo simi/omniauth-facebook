0106bb4b15c5d2ffb27331fb45ba161a7f007b0d
Mark Dodwell <mark@mkdynamic.co.uk>
2011-10-15 19:42:12 -0700

    initial implementation for facebook strategy

--- lib/omniauth/strategies/facebook.rb
+++ lib/omniauth/strategies/facebook.rb
@@ -3,7 +3,29 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class Facebook < OmniAuth::Strategies::OAuth2
-      include OmniAuth::Strategy
+      option :client_options, {
+        :site => 'https://graph.facebook.com',
+        :token_url => '/oauth/access_token'
+      }
+
+      option :token_params, {
+        :parse => :query
+      }
+
+      option :access_token_options, {
+        :header_format => 'OAuth %s',
+        :param_name => 'access_token'
+      }
+
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end
+
+      def access_token_options
+        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
+      end
     end
   end
 end
--- spec/omniauth/strategies/facebook_spec.rb
+++ spec/omniauth/strategies/facebook_spec.rb
@@ -2,4 +2,45 @@ require 'spec_helper'
 require 'omniauth-facebook'
 
 describe OmniAuth::Strategies::Facebook do
+  subject do
+    OmniAuth::Strategies::Facebook.new(nil, @options || {})
+  end
+
+  it_should_behave_like 'an oauth2 strategy'
+
+  describe '#client' do
+    it 'has correct Facebook site' do
+      subject.client.site.should eq('https://graph.facebook.com')
+    end
+
+    it 'has correct authorize url' do
+      subject.client.options[:authorize_url].should eq('/oauth/authorize')
+    end
+
+    it 'has correct token url' do
+      subject.client.options[:token_url].should eq('/oauth/access_token')
+    end
+  end
+
+  describe '#authorize_params' do
+    it 'is empty by default' do
+      subject.authorize_params.should be_empty
+    end
+  end
+
+  describe '#token_params' do
+    it 'has correct parse strategy' do
+      subject.token_params[:parse].should eq(:query)
+    end
+  end
+
+  describe '#access_token_options' do
+    it 'has correct param name by default' do
+      subject.access_token_options[:param_name].should eq('access_token')
+    end
+
+    it 'has correct header format by default' do
+      subject.access_token_options[:header_format].should eq('OAuth %s')
+    end
+  end
 end
