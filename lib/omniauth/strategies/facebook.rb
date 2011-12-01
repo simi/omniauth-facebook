8086931ba1bb6da9c18c302a140a7023f704f37e
Mark Dodwell <mark@mkdynamic.co.uk>
2011-11-30 19:06:30 -0800

    support for parsing code from signed request cookie

--- lib/omniauth/strategies/facebook.rb
+++ lib/omniauth/strategies/facebook.rb
@@ -66,8 +66,10 @@ module OmniAuth
       end
 
       def build_access_token
-        super.tap do |token|
-          token.options.merge!(access_token_options)
+        with_code(request.params['code'] || signed_request && signed_request['code']) do
+          super.tap do |token|
+            token.options.merge!(access_token_options)
+          end
         end
       end
 
@@ -81,14 +83,55 @@ module OmniAuth
           params[:scope] ||= DEFAULT_SCOPE
         end
       end
+
+      def signed_request
+        @signed_request ||= begin
+          cookie = request.cookies["fbsr_#{client.id}"] and
+          parse_signed_request(cookie)
+        end
+      end
       
       private
+      
+      def with_code(code)
+        original_code = request.params['code']
+        begin
+          request.params['code'] = code
+          yield
+        ensure
+          request.params['code'] = original_code
+        end
+      end
       
       def prune!(hash)
         hash.delete_if do |_, value| 
           prune!(value) if value.is_a?(Hash)
           value.nil? || (value.respond_to?(:empty?) && value.empty?)
         end
+      end
+      
+      def parse_signed_request(value)
+        signature, encoded_payload = value.split('.')
+
+        decoded_hex_signature = base64_decode_url(signature)#.unpack('H*')
+        decoded_payload = MultiJson.decode(base64_decode_url(encoded_payload))
+
+        unless decoded_payload['algorithm'] == 'HMAC-SHA256'
+          raise NotImplementedError, "unkown algorithm: #{decoded_payload['algorithm']}"
+        end
+
+        if valid_signature?(client.secret, decoded_hex_signature, encoded_payload)
+          decoded_payload
+        end
+      end
+
+      def valid_signature?(secret, signature, payload, algorithm = OpenSSL::Digest::SHA256.new)
+        OpenSSL::HMAC.digest(algorithm, secret, payload) == signature
+      end
+
+      def base64_decode_url(value)
+        value += '=' * (4 - value.size.modulo(4))
+        Base64.decode64(value.tr('-_', '+/'))
       end
     end
   end
