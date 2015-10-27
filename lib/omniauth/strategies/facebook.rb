64b1603e9446097491ec7a4b70ad505d2eb43a7b
Mark Dodwell <markdodwell@gmail.com>
2015-10-26 23:07:40 -0700

    add note

--- lib/omniauth/strategies/facebook.rb
+++ lib/omniauth/strategies/facebook.rb
@@ -83,6 +83,7 @@ module OmniAuth
         if @authorization_code_from_signed_request_in_cookie
           ''
         else
+          # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
           options[:callback_url] || (full_host + script_name + callback_path)
         end
       end
