9bd8bd57622b7a3437ee83e9761ac9524efeb2a3
Fabian Winkler <wynk+buhtig@handgemen.ge>
2024-04-18 21:33:44 +0200

    Update API version to v19.0

--- lib/omniauth/strategies/facebook.rb
+++ lib/omniauth/strategies/facebook.rb
@@ -10,7 +10,7 @@ module OmniAuth
       class NoAuthorizationCodeError < StandardError; end
 
       DEFAULT_SCOPE = 'email'
-      DEFAULT_FACEBOOK_API_VERSION = 'v5.0'.freeze
+      DEFAULT_FACEBOOK_API_VERSION = 'v19.0'.freeze
 
       option :client_options, {
         site: "https://graph.facebook.com/#{DEFAULT_FACEBOOK_API_VERSION}",
